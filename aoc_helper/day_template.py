4286a0c530ed6347e12047592196a88a01756f24
starwort <tcphone93@gmail.com>
2020-12-06 17:38:48 +0000

    Format day_template.py

--- aoc_helper/day_template.py
+++ aoc_helper/day_template.py
@@ -2,16 +2,21 @@ import aoc_helper
 
 raw = aoc_helper.fetch({day}, {year})
 
+
 def parse_raw():
     ...
 
+
 data = parse_raw()
+
 
 def part_one():
     ...
 
+
 def part_two():
     ...
+
 
 aoc_helper.lazy_submit(day={day}, year={year}, solution=part_one)
 aoc_helper.lazy_submit(day={day}, year={year}, solution=part_two)
