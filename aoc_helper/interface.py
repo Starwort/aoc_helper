cd527a3ab7856a3f6db28e7d4820c2214f6a70ee
salt-die <53280662+salt-die@users.noreply.github.com>
2020-12-06 06:08:43 -0600

    Private functions renamed with leading underscore. Simplify pretty printing with f-strings and esoteric yet well-commented _pretty_print function. Cache ALL submissions in a json file! But still keep solution separate. Questionable optimizations included: please read comments.

--- aoc_helper/interface.py
+++ aoc_helper/interface.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import pathlib
 import time
 import typing
@@ -9,79 +10,79 @@ from bs4 import BeautifulSoup as Soup
 
 try:
     import colorama as colour
-
-    colour.init()
-    RED = colour.Fore.LIGHTRED_EX
-    YELLOW = colour.Fore.LIGHTYELLOW_EX
-    GREEN = colour.Fore.LIGHTGREEN_EX
-    BLUE = colour.Fore.LIGHTBLUE_EX
-    RESET = colour.Fore.RESET
 except ImportError:
     RED = ""
     YELLOW = ""
     GREEN = ""
     BLUE = ""
     RESET = ""
+else:
+    colour.init()
+    RED = colour.Fore.LIGHTRED_EX
+    YELLOW = colour.Fore.LIGHTYELLOW_EX
+    GREEN = colour.Fore.LIGHTGREEN_EX
+    BLUE = colour.Fore.LIGHTBLUE_EX
+    RESET = colour.Fore.RESET
 
 from .data import DATA_DIR, DEFAULT_YEAR, URL, WAIT_TIME, get_cookie
 
 
-def make_if_not_exists(folder: pathlib.Path):
+def _make(folder: pathlib.Path) -> None:
+    """Create folder if it doesn't exist."""
     if not folder.exists():
         folder.mkdir(parents=True)
 
 
-def analyse_and_print(message: str) -> None:
-    if message.startswith("That's the"):
-        print(GREEN + message + RESET)
-    elif message.startswith("You don't"):
-        print(YELLOW + message + RESET)
-    elif message.startswith("That's not"):
-        print(RED + message + RESET)
-    else:
-        raise ValueError("Failed to parse response.")
+def _pretty_print(message: str) -> None:
+    # The first index where each server response has a different character is index 7.
+    # We differentiate messages with this index.
+    #  *  "green" indicates a correct answer
+    #  *  "yellow" indicates a submission to an already solved problem
+    #  *  "red" indicates a timeout or wrong answer
+    try:
+        color = {"t": GREEN, "'": YELLOW, "n": RED, "e": RED, " ": YELLOW, "g": GREEN}[message[7]]
+    except KeyError as e:
+        raise ValueError("Failed to parse response.") from e
+    print(f"{color}{message}{RESET}")
 
 
-def fetch_data(day: int, year: int = DEFAULT_YEAR) -> str:
+def fetch(day: int, year: int = DEFAULT_YEAR) -> str:  # Might consider a default TODAY for day
     """Fetch and return the input for `day` of `year`.
 
     All inputs are cached in `aoc_helper.DATA_DIR`."""
     day_ = str(day)
     year_ = str(year)
 
-    make_if_not_exists(DATA_DIR / year_)
+    _make(DATA_DIR / year_)
     input_path = DATA_DIR / year_ / (day_ + ".in")
 
     if input_path.exists():
-        with open(input_path) as file:
-            return file.read()
+        return input_path.read_text()
     else:
         unlock = datetime.datetime(year, 12, day, 5)
         now = datetime.datetime.utcnow()
         if now < unlock:
-            print(YELLOW + "Waiting for puzzle unlock..." + RESET)
+            _pretty_print("Waiting for puzzle unlock...")  # Rather esoteric checks in _pretty_print to
             time.sleep((unlock - now).total_seconds())
-            print(GREEN + "Fetching input!" + RESET)
+            _pretty_print("Fetching input!")               # distinguish between these messages.
         resp = requests.get(
             URL.format(day=day, year=year) + "/input", cookies=get_cookie()
         )
         if not resp.ok:
             raise ValueError("Received bad response")
+        # Note to star: May consider rstrip instead -- I don't know if AoC will ever
+        # publish input that has significant whitespace at the beginning though.
         data = resp.text.strip()
-        with open(input_path, "w") as file:
-            file.write(data)
+        input_path.write_text(data)
         return data
 
 
-day = fetch_data  # alias for old interface
-
-
-def submit_answer(
+def submit(
     day: int, part: int, answer: typing.Any, year: int = DEFAULT_YEAR
 ) -> None:
-    """Submit an answer.
+    """Submit a solution.
 
-    Submissions are cached; submitting the previous answer will return the
+    Submissions are cached; submitting an already submitted solution will return the
     previous response.
     """
     day_ = str(day)
@@ -89,57 +90,41 @@ def submit_answer(
     part_ = str(part)
     answer_ = str(answer)
 
-    make_if_not_exists(DATA_DIR / year_)
-    path_base = DATA_DIR / year_
+    submission_dir = DATA_DIR / year_ / day_
+    _make(submission_dir)
 
-    if (path_base / f"{part}.solution").exists():  # empty flag file
+    # Load cached solutions
+    submissions = submission_dir / "submissions.json"
+    if submissions.exists():
+        with submissions.open() as f:
+            solutions = json.load(f)
+    else:
+        solutions = {"1": {}, "2": {}}
+
+    # Check if solved
+    solution_file = submission_dir / f"{part}.solution"
+    if solution_file.exists():
+        solution = solutions_file.read_text()
         print(
-            "Day "
-            + BLUE
-            + day_
-            + RESET
-            + " part "
-            + BLUE
-            + part_
-            + RESET
-            + " has already been solved.\nThe solution was: "
-            + BLUE
-            + (path_base / f"{part}.out").read_text()
+            f"Day {BLUE}{day}{RESET} part {BLUE}{part}{RESET} "
+            f"has already been solved.\nThe solution was: "
+            f"{BLUE}{solution}{RESET}\nResponse was:\n"
         )
-        return
+        return _pretty_print(solutions[part_][solution])
 
-    if answer_ == (path_base / f"{part}.out").read_text():
+    # Check if answer has already been submitted
+    if answer_ in solutions[part_]:
         print(
-            YELLOW
-            + "Solution "
-            + BLUE
-            + answer_
-            + YELLOW
-            + " to part "
-            + BLUE
-            + part_
-            + YELLOW
-            + " was your last submission."
-            + RESET
+            f"{YELLOW}Solution: {BLUE}{answer}{YELLOW} to part "
+            f"{BLUE}{part}{YELLOW} has already been submitted.\n"
+            "Response was:\n{RESET}"
         )
-        analyse_and_print((path_base / f"{part}.resp").read())
-        return
+        return _pretty_print(solutions[part_][answer_])
 
-    (path_base / f"{part}.out").write_text(answer_)
-
-    submitted = False
-    msg = resp = None
-    while not submitted:
+    while True:
         print(
-            "Submitting "
-            + BLUE
-            + answer_
-            + RESET
-            + " as the solution to part "
-            + BLUE
-            + part_
-            + RESET
-            + "..."
+            f"Submitting {BLUE}{answer}{RESET} as the solution to part "
+            f"{BLUE}{part}{RESET}..."
         )
         resp = requests.post(
             url=URL.format(day=day, year=year) + "/answer",
@@ -148,30 +133,29 @@ def submit_answer(
         )
         if not resp.ok:
             raise ValueError("Received bad response")
+
         msg: str = Soup(resp.text, "html.parser").article.text
+        _pretty_print(msg)
 
-        if msg.startswith("You gave"):
-            print(RED + msg + RESET)
+        if msg[4] == "g":  # A quick check to see if the message starts with "You gave ..."
             wait_match = WAIT_TIME.search(msg)
-
             pause = 60 * int(wait_match[1] or 0) + int(wait_match[2])
             print(
-                YELLOW
-                + "Waiting "
-                + BLUE
-                + str(pause)
-                + YELLOW
-                + " seconds to retry..."
+                f"{YELLOW}Waiting {BLUE}{pause}{YELLOW} seconds to retry...{RESET}""
             )
             time.sleep(pause)
         else:
-            submitted = True
-    analyse_and_print(msg)
-    if msg.startswith("That's the"):
-        (path_base / f"{part}.solution").touch()  # create the flag file
+            break
+
+    if msg[7] == "t":  # "That's the right answer!", note index 7 differentiates between all other responses.
+        solution_file.write_text(answer_)
         if part == 1:
             webbrowser.open(resp.url)  # open part 2 in the user's browser
-    (path_base / f"{part}.resp").write_text(msg)
+
+    # Cache submission
+    solutions[part_][answer_] = msg
+    with submissions.open() as f:
+        json.dump(solutions, f)
 
 
 def run_and_submit(
