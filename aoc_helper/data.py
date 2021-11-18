c6108a7557eb08029a8aa285b30e3c496b2a7c25
starwort <tcphone93@gmail.com>
2021-11-18 10:49:50 +0000

    vi adds a newline if you change your token manually

--- aoc_helper/data.py
+++ aoc_helper/data.py
@@ -14,7 +14,7 @@ WAIT_TIME = re.compile(r"You have (?:(\d+)m )?(\d+)s left to wait.")
 def get_cookie():
     token_file = DATA_DIR / "token.txt"
     if token_file.exists():
-        return {"session": token_file.read_text()}
+        return {"session": token_file.read_text().strip('\n')}
     token = input("Could not find configuration file. Please enter your token\n>>> ")
     token_file.write_text(token)
     return {"session": token}
