4ef6aeab7fc21354470c9bc46c4199d1ce2a7d14
starwort <tcphone93@gmail.com>
2021-12-01 19:15:45 +0000

    Add (and fix) docstrings for utils, add sum method

--- aoc_helper/utils.py
+++ aoc_helper/utils.py
@@ -22,8 +22,8 @@ def chunk(
 ) -> typing.Iterable[tuple[T, ...]]:
     """Utility function to chunk an iterable into chunks of a given size.
 
-    Assumes your iterable produces a number of items that is a multiple of the
-    chunk size.
+    If there are not enough elements in the iterable to fill the last chunk,
+    the last chunk will be dropped.
     """
     return zip(*[builtins.iter(iterable)] * chunk_size)
 
@@ -33,8 +33,8 @@ def chunk_default(
 ) -> typing.Iterable[tuple[T, ...]]:
     """Utility function to chunk an iterable into chunks of a given size.
 
-    Assumes your iterable produces a number of items that is a multiple of the
-    chunk size.
+    If there are not enough elements in the iterable to fill the last chunk,
+    the missing elements will be replaced with the default value.
     """
     return itertools.zip_longest(
         *[builtins.iter(iterable)] * chunk_size, fillvalue=default
@@ -51,11 +51,20 @@ class SmartIterator(typing.Generic[T]):
         return self.it.__iter__()
 
     def map(self, func: typing.Callable[[T], U]) -> "SmartIterator[U]":
+        """Return an iterator containing the result of calling func on each
+        element in this iterator.
+        """
         return type(self)(map(func, self))
 
     def filter(
         self, pred: typing.Union[typing.Callable[[T], bool], None]
     ) -> "SmartIterator[T]":
+        """Return an iterator containing only the elements for which pred
+        returns True.
+
+        If pred is None, return an iterator containing only elements that are
+        truthy.
+        """
         return type(self)(filter(pred, self))
 
     @typing.overload
@@ -71,21 +80,32 @@ class SmartIterator(typing.Generic[T]):
         ...
 
     def reduce(self, func, initial=_SENTINEL):
+        """Reduce the iterator to a single value, using the reduction
+        function provided.
+        """
         if initial is self._SENTINEL:
             return functools.reduce(func, self)
-        else:
-            return functools.reduce(func, self, initial)
+        return functools.reduce(func, self, initial)
 
     def foreach(self, func: typing.Callable[[T], typing.Any]) -> None:
+        """Run func on every value in this iterator, immediately."""
         for el in self:
             func(el)
 
     def chunk(self, n: int) -> "SmartIterator[typing.Tuple[T, ...]]":
+        """Return an iterator containing the elements of this iterator in chunks
+        of size n. If there are not enough elements to fill the last chunk, it
+        will be dropped.
+        """
         return type(self)(chunk(self, n))
 
     def chunk_default(
         self, n: int, default: T
     ) -> "SmartIterator[typing.Tuple[T, ...]]":
+        """Return an iterator containing the elements of this iterator in chunks
+        of size n. If there are not enough elements to fill the last chunk, the
+        missing elements will be replaced with the default value.
+        """
         return type(self)(chunk_default(self, n, default))
 
     def _window(
@@ -106,28 +126,67 @@ class SmartIterator(typing.Generic[T]):
             yield tuple(elements)
 
     def window(self, window_size: int) -> "SmartIterator[typing.Tuple[T, ...]]":
+        """Return an iterator containing the elements of this iterator in
+        a sliding window of size window_size. If there are not enough elements
+        to create a full window, the iterator will be empty.
+        """
         return type(self)(self._window(window_size))
 
     def next(self) -> T:
+        """Return the next element in the iterator, or raise StopIteration."""
         return next(self.it)
 
     def skip(self, n: int = 1) -> "SmartIterator[T]":
+        """Skip and discard n elements from this iterator.
+
+        Raises StopIteration if there are not enough elements.
+        """
         for _ in builtins.range(n):
             self.next()
         return self
 
     def nth(self, n: int) -> T:
+        """Return the nth element of this iterator.
+
+        Discards all elements up to the nth element, and raises StopIteration
+        if there are not enough elements.
+        """
         self.skip(n)
         return self.next()
 
     def take(self, n: int) -> typing.Tuple[T, ...]:
+        """Return the next n elements of this iterator.
+
+        Raises StopIteration if there are not enough elements.
+        """
         return tuple(self.next() for _ in builtins.range(n))
 
     def collect(self) -> "typing.List[T]":
+        """Return a list containing all remaining elements of this iterator."""
         return list(self)
 
     def chain(self, other: typing.Iterable[T]) -> "SmartIterator[T]":
+        """Return an iterator containing the elements of this iterator followed
+        by the elements of other.
+        """
         return type(self)(itertools.chain(self, other))
+
+    @typing.overload
+    def sum(self) -> T:
+        ...
+
+    @typing.overload
+    def sum(self, initial: T) -> T:
+        ...
+
+    def sum(self, initial=_SENTINEL):
+        """Return the sum of all elements in this iterator.
+
+        If initial is provided, it is used as the initial value.
+        """
+        if initial is self._SENTINEL:
+            return sum(self)
+        return sum(self, initial)
 
 
 @functools.wraps(range)
