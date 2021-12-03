b612db07bd9198664d4bf78d830e32d4be177eb6
starwort <tcphone93@gmail.com>
2021-12-03 04:55:22 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aoc_helper",
-    version="1.2.3",
+    version="1.2.4",
     author="Starwort, salt-die",
     description="A helper package for Advent of Code",
     long_description=long_description,
