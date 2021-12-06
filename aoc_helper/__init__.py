4dbc7a76bdb4e500199a729c0d59d31b78f865b0
starwort <tcphone93@gmail.com>
2021-12-06 04:58:18 +0000

    Properly import irange and frange

--- aoc_helper/__init__.py
+++ aoc_helper/__init__.py
@@ -1,2 +1,2 @@
 from .interface import fetch, lazy_submit, submit
-from .utils import chunk, chunk_default, extract_ints, iter, list, range
+from .utils import chunk, chunk_default, extract_ints, frange, irange, iter, list, range
