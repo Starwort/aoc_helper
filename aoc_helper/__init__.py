efb5f6a6c3c90518b5752d01957b976c57df6df5
starwort <tcphone93@gmail.com>
2021-12-07 04:56:47 +0000

    Remember to export tail_call

--- aoc_helper/__init__.py
+++ aoc_helper/__init__.py
@@ -1,2 +1,12 @@
 from .interface import fetch, lazy_submit, submit
-from .utils import chunk, chunk_default, extract_ints, frange, irange, iter, list, range
+from .utils import (
+    chunk,
+    chunk_default,
+    extract_ints,
+    frange,
+    irange,
+    iter,
+    list,
+    range,
+    tail_call,
+)
