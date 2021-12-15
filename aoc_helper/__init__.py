9e1c864d585c61d3c09b1d8518530fb1f7ad4cce
starwort <tcphone93@gmail.com>
2021-12-15 05:51:32 +0000

    Change init to automatically import everything

--- aoc_helper/__init__.py
+++ aoc_helper/__init__.py
@@ -1,14 +1,2 @@
 from .interface import fetch, lazy_submit, submit
-from .utils import (
-    chunk,
-    chunk_default,
-    decode_text,
-    extract_ints,
-    frange,
-    irange,
-    iter,
-    list,
-    map,
-    range,
-    tail_call,
-)
+from .utils import *
